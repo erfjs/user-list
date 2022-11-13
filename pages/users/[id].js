5fb13830309112006bd1a6dcca5656bfa8ac07a1
erfii <erfan.granmayeh@gmail.com>
2022-11-13 18:27:57 +0330

    log id

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -19,7 +19,7 @@ export const getStaticProps = async (context) => {
   const id = context.params.id;
   const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
   const data = await res.json();
-
+  console.log(data);
   return {
     props: { user: data },
   };
